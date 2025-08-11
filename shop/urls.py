cb138d9992cf42b46bba06048589e0e002df5928
song <song@sesac.com>
2025-08-11 18:10:58 +0900

    ㅇㄴ

--- shop/urls.py
+++ shop/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('main/', views.shoplist, name='main'),
     path('mypage/order_status/', views.order_status, name='order_status'),
-    path('mypage/order_history/', views.order_history, name='order_history'),
     path('mypage/wishlist/', views.wishlist, name='wishlist'),
     path('mypage/contact/', views.contact, name='contact'),
     path('mypage/contact_history/', views.contact_history, name='contact_history'),
